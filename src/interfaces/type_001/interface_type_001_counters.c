bc85ef04c7e3df1d59c977d039cbfcea8d73118d
Patrice Dietsch <patrice.dietsch@gmail.com>
2019-12-31 21:46:36 +0100

    correction

--- src/interfaces/type_001/interface_type_001_counters.c
+++ src/interfaces/type_001/interface_type_001_counters.c
@@ -250,7 +250,6 @@ int16_t counter_read(interface_type_001_t *i001, struct electricity_counter_s *c
          }
          retry++;
       }
-
    }
    while(ret && retry<5);
 
@@ -259,9 +258,11 @@ int16_t counter_read(interface_type_001_t *i001, struct electricity_counter_s *c
       counter->wh_counter=c;
       counter->kwh_counter=c / 1000;
       counter->counter=c;
+      return 0;
    }
-   
-   return 0;
+   else {
+      return -1;
+   }
 }
 
 
@@ -335,11 +336,9 @@ int16_t interface_type_001_counters_poll_inputs2(interface_type_001_t *i001)
    struct electricity_counter_s *counter;
 
    mea_queue_first(counters_list);
-   VERBOSE(9) mea_log_printf("%s (%s) : start counters polling\n", INFO_STR, __func__);
    for(int16_t i=0; i<counters_list->nb_elem; i++) {
       mea_queue_current(counters_list, (void **)&counter);
 
-      VERBOSE(9) mea_log_printf("%s (%s) : polling counter %s / %d ?\n", INFO_STR, __func__, counter->name, (long)counter->counter);
       pthread_cleanup_push((void *)pthread_mutex_unlock, (void *)&(counter->lock));
       pthread_mutex_lock(&(counter->lock));
       
